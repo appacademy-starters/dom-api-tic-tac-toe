4554524323f6dae9b379942c0921e0c63edc9ad6
Marc Carlson <marc.carlson30@gmail.com>
2020-10-01 17:19:57 -0500

    Add checkGameStatus

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,20 @@
 window.addEventListener("DOMContentLoaded", (e) => {
-    let currentPlayerSymbol  = "X";
+    let currentPlayerSymbol  = "x";
     const squareValues = ["","","","","","","","",""];
     const squares = document.querySelectorAll(".square");
+    const gameStatus = "";
+
+    function checkGameStatus(){
+        const newArray = [];
+        const checkRow1 = squareValues[0] + squareValues[1] + squareValues[2];
+        const checkRow2 = squareValues[3] + squareValues[4] + squareValues[5];
+        const checkRow3 = squareValues[6] + squareValues[7] + squareValues[8];
+        const checkColumn1 = squareValues[0] + squareValues[3] + squareValues[6];
+        const checkColumn2 = squareValues[1] + squareValues[4] + squareValues[7];
+        const checkColumn3 = squareValues[2] + squareValues[5] + squareValues[8];
+        const checkDiagonal1 = squareValues[0] + squareValues[4] + squareValues[8];
+        const checkDiagonal2 = squareValues[2] + squareValues[4] + squareValues[6];
+    }
 
     const grid = document.getElementById("tic-tac-toe-board");
 
@@ -10,7 +23,15 @@ window.addEventListener("DOMContentLoaded", (e) => {
         if (gridId.startsWith("square-")) {
             const sqIndex = Number.parseInt(gridId.slice(7));
             if (squareValues[sqIndex] === "") {
-
+                const img = document.createElement("img");
+                img.setAttribute("src", `player-${currentPlayerSymbol}.svg`);
+                e.target.appendChild(img);
+                squareValues[sqIndex] = currentPlayerSymbol;
+                if(currentPlayerSymbol === "x"){
+                    currentPlayerSymbol = "o";
+                } else{
+                    currentPlayerSymbol = "x";
+                }
             }
         }
     })
