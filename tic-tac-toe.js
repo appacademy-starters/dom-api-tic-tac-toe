b93d94d92e8022d3c4904d91aa13daf91734cf98
James Lentzsch <jrlentzsch@gmail.com>
2021-02-04 16:02:05 -0800

    Psuedo Code

---
+++ tic-tac-toe.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /* 
+    1 add event listener to each square
+    2 when square is clicked set square to appropriate immage
+        -run check on class, if does does include "square"
+            replace square element with appriate immage, set class to
+            said immage.
+    */
+
+    
+})
